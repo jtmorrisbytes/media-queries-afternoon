70b872a8dde3097d9cd36b62452f588376248e39
= <=>
2020-06-18 18:21:46 -0500

    ready to merge into master

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ function App() {
         <Navbar.Brand src={logo} alt="Start Bootstrap" />
         <Navbar.Toggle>Menu</Navbar.Toggle>
         <Navbar.Collapse>
-          <Navbar.Link href="https://www.google.com">item 1</Navbar.Link>
-          <Navbar.Link href="https://www.google.com">item 2</Navbar.Link>
-          <Navbar.Link href="https://www.google.com">item 3</Navbar.Link>
+          <Navbar.Link href="https://www.google.com">Services</Navbar.Link>
+          <Navbar.Link href="https://www.google.com">Portfolio</Navbar.Link>
+          <Navbar.Link href="https://www.google.com">About</Navbar.Link>
+          <Navbar.Link href="https://www.google.com">Team</Navbar.Link>
+          <Navbar.Link href="https://www.google.com">Contact</Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
       {/* <Header /> */}
